e799359667dce6b9de2335a4e572e0e0ff095965
Andrew Jung <creaconts@gmail.com>
2020-03-02 20:44:04 +0900

    no message

--- Test/test.py
+++
@@ -1,9 +0,0 @@
-import ssl
-import urllib.request
-
-context = ssl._create_unverified_context()
-
-url = 'https://www.naver.com'
-html = urllib.request.urlopen(url, context=context)
-print(html.read())
-
